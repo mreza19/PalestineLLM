9b47cfc7dacb04830c114ae9daf7201f8cfa3451
morteza <m@gmail.com>
2024-10-08 23:49:42 +0330

    add tel2

--- telegram2/main.js
+++ telegram2/main.js
@@ -8,7 +8,7 @@ import OllamaChat from "./ollama.js";
 
 dotenv.config();
 
-const chat = new OllamaChat('http://127.0.0.1:11434', 'unsloth_model');
+const chat = new OllamaChat(process.env.HOST || 'http://127.0.0.1:11434', process.env.MODEL || 'unsloth_model');
 chat.setSystemMessage("you should only answer using emjies and nothing else");
 
 
