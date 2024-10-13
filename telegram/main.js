91500c773ce0ce008e3228fb0f631097df7844c4
morteza <m@gmail.com>
2024-10-13 20:48:18 +0330

    move code from tel2

--- telegram/main.js
+++ telegram/main.js
@@ -28,43 +28,65 @@ const rl = readline.createInterface({
 	const client = new TelegramClient(stringSession, apiId, apiHash, {
 		connectionRetries: 5,
 	});
-	await client.connect();
-	// await client.start({
-	//   phoneNumber: async () =>
-	//     new Promise((resolve) =>
-	//       rl.question("Please enter your number: ", resolve)
-	//     ),
-	//   password: async () =>
-	//     new Promise((resolve) =>
-	//       rl.question("Please enter your password: ", resolve)
-	//     ),
-	//   phoneCode: async () =>
-	//     new Promise((resolve) =>
-	//       rl.question("Please enter the code you received: ", resolve)
-	//     ),
-	//   onError: (err) => console.log(err),
-	// });
-	// console.log("You should now be connected.");
-	// console.log(client.session.save()); // Save this string to avoid logging in again
-	// await client.sendMessage("me", { message: "Hello!" });
+	if (process.env.SESSION) {
+		console.log("connectiong...");
+		await client.connect();
+		console.log("Connected successfully");
+		let resMe = await client.getMe();
+		console.log({ firstName: resMe.firstName });
+	}
+	else {
+		await client.start({
+			phoneNumber: async () =>
+				new Promise((resolve) =>
+					rl.question("Please enter your number: ", resolve)
+				),
+			password: async () =>
+				new Promise((resolve) =>
+					rl.question("Please enter your password: ", resolve)
+				),
+			phoneCode: async () =>
+				new Promise((resolve) =>
+					rl.question("Please enter the code you received: ", resolve)
+				),
+			onError: (err) => console.log(err),
+		});
+		console.log("You should now be connected.");
+		console.log("Save this string to avoid logging in again:");
+		console.log(client.session.save());
+		console.log("Put this string to env.SESSION");
+		await client.sendMessage("me", { message: "Hello!" });
+	}
 
 	let users = {};
 
 	async function handler(event) {
 		const eventt = new NewMessageEvent(event);
-			console.log(eventt.message.message.peerId.className)
-			console.log(eventt.message.message.message)
-			if(eventt.message.message.peerId.className == 'PeerUser' && eventt.message.message.message){
-				if(!users[eventt.message.message.peerId.userId.value]){
-					let chat = await client.getDialogs();
-					users[eventt.message.message.peerId.userId.value] = 1;
-				}
-				let userMessage = eventt.message.message.message;
-				const response = await chat.chatWithModel(userMessage);  // Send the user's message to the model
-								
-				// Send the response back to the user
-				await client.sendMessage(eventt.message.message.peerId.userId.value, { message: response.content });
+		console.log(eventt.message.message.peerId.className)
+		console.log(eventt.message.message.message)
+		if (eventt.message.message.peerId.className == 'PeerUser' && eventt.message.message.message) {
+			if (!users[eventt.message.message.peerId.userId.value]) {
+				let chat = await client.getDialogs();
+				users[eventt.message.message.peerId.userId.value] = 1;
 			}
+			let message = eventt.message.message.message;
+			const response = await chat.chatWithModel(JSON.stringify({
+				userId: eventt.message.message.peerId.userId.value,
+				isChannel: eventt.message.message.isChannel,
+				isGroup: eventt.message.message.isGroup,
+				isPrivate: eventt.message.message.isPrivate,
+				msgId: eventt.message.message.id,
+				className: eventt.message.message.peerId.className,
+				message
+			}, (key, value) =>
+				typeof value === 'bigint'
+					? value.toString()
+					: value // return everything else unchanged
+			));  // Send the user's message to the model
+
+			// Send the response back to the user	
+			await client.sendMessage(eventt.message.message.peerId.userId.value, { message: response.content });
 		}
+	}
 	client.addEventHandler(handler, new NewMessage({}));
 })();
